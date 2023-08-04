7fc8e153603bd59340965e6a1efa7565d4fe4016
Aniket Prabhu <aniprabhu@gmail.com>
2023-08-03 20:21:14 -0700

    rehauled task 1 code

--- code/task_1/project_2a_task1_ANP.py
+++ code/task_1/project_2a_task1_ANP.py
@@ -11,88 +11,71 @@ import numpy as np
 import cv2
 from os import chdir
 
-''' Loading images and extracting 3D-to-2D point correspondences '''
-# Saving path to avoid calling it repeatedly:
-currwd = __file__  # Current working directory
-path = currwd.replace("code/task_1/project_2a_task1_ANP.py", "images/task_1/")
+def calibrate_camera(left_or_right, images, path):
+    # Lists for storing 2D (image) points:
+    cornarray = []
 
-# path = r'/home/whizbuzzer/CSE 598/Assignments/Project 2a/project_2a/images/task_1/'
+    # To create 3d (object) points:
+    objpoints = np.zeros((6 * 9, 3), np.float32)
+    objpoints[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
 
-# Storing image names in a list to call them later in the for loop:
-images_l = ["left_0.png", "left_1.png", "left_2.png", "left_3.png", "left_4.png",
-            "left_5.png", "left_6.png", "left_7.png", "left_8.png", "left_9.png", "left_10.png"]        # left
-images_r = ["right_0.png", "right_1.png", "right_2.png", "right_3.png", "right_4.png",
-            "right_5.png", "right_6.png", "right_7.png", "right_8.png", "right_9.png", "right_10.png"]  # right
+    # Lists for strong 3D points:
+    objp_array = []
 
-# Only one n_images need since both left and right have equal n(image)
-n_images = len(images_l) 
+    # Finding object points and image points for left camera:
+    for i in range(len(images)):
+        img = cv2.imread(path+images[i])
+        grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Gets rid of RGB data which is not required to find points
+        h, w = img.shape[:2]  # 480x640
+        retval, corners = cv2.findChessboardCorners(grey, (9,6), None)
+        if retval == 1:
+            objp_array.append(objpoints)
+            cornarray.append(corners)
 
-# Lists for storing 2D (image) points:
-cornarray_l = []  
-cornarray_r = []
+    '''Calculating camera intrinsic parameters for left camera: '''
+    ter, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objp_array, cornarray, grey.shape[::-1], None, None)
 
-# To create 3d (object) points:
-objpoints = np.zeros((6*9, 3), np.float32)
-objpoints[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+    if left_or_right == "l":
+        xml_file = "left_camera_intrinsics.xml"
+    elif left_or_right == "r":
+        xml_file = "right_camera_intrinsics.xml"
 
-# Lists for strong 3D points:
-objp_array_l = []
-objp_array_r = []
+    cam_params = cv2.FileStorage(currwd.replace("code/task_1/project_2a_task1_ANP.py", "parameters/" + xml_file), cv2.FileStorage_WRITE)
+    cam_params.write("Intrinsic_Matrix", mtx)
+    cam_params.write("Distortion_Coefficients", dist)
 
-# Finding object points and image points for left camera:
-for i in range(n_images):
-    limg = cv2.imread(path+images_l[i])
-    grey_l = cv2.cvtColor(limg, cv2.COLOR_BGR2GRAY)
-    h_l, w_l = limg.shape[:2]  # 480x640
-    retval_l, corners_l = cv2.findChessboardCorners(grey_l, (9,6), None)
-    if retval_l == 1:
-        objp_array_l.append(objpoints)
-        cornarray_l.append(corners_l)
+    '''Checking calibration results for left camera: '''
+    newcammat, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 0, (w, h))
+    mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcammat, (w, h), 5)
+    img = cv2.imread(path + images[2])  # left_2.png
+    dist = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
+    x, y, w, h = roi
+    dist = dist[y:y + h, x:x + w]
+    return dist
 
-'''Calculating camera intrinsic parameters for left camera: '''
-ter_l, mtx_l, dist_l, rvecs_l, tvecs_l = cv2.calibrateCamera(objp_array_l, cornarray_l, grey_l.shape[::-1], None, None)
 
-l_cam_params = cv2.FileStorage(currwd.replace("code/task_1/project_2a_task1_ANP.py", "parameters/left_camera_intrinsics.xml"), cv2.FileStorage_WRITE)
-l_cam_params.write("Intrinsic_Matrix", mtx_l)
-l_cam_params.write("Distortion_Coefficients", dist_l)
+if __name__ == "__main__":
+    ''' Loading images and extracting 3D-to-2D point correspondences '''
+    # Saving path to avoid calling it repeatedly:
+    currwd = __file__  # Current working directory
+    path = currwd.replace("code/task_1/project_2a_task1_ANP.py", "images/task_1/")
 
-'''Checking calibration results for left camera: '''
-newcammat_l, roi_l = cv2.getOptimalNewCameraMatrix(mtx_l, dist_l, (w_l, h_l), 0, (w_l, h_l))
-mapx_l, mapy_l = cv2.initUndistortRectifyMap(mtx_l, dist_l, None, newcammat_l, (w_l, h_l), 5)
-limg = cv2.imread(path+images_l[2])  # left_2.png
-dist_l = cv2.remap(limg, mapx_l, mapy_l, cv2.INTER_LINEAR)
-x_l, y_l, w_l, h_l = roi_l
-dist_l = dist_l[y_l:y_l+h_l, x_l:x_l+w_l]
+    # path = r'/home/whizbuzzer/CSE 598/Assignments/Project 2a/project_2a/images/task_1/'
 
-# Finding object points and image points for right camera:
-for i in range(n_images):
-    rimg = cv2.imread(path+images_r[i])
-    grey_r = cv2.cvtColor(rimg, cv2.COLOR_BGR2GRAY)
-    h_r, w_r = rimg.shape[:2]
-    retval_r, corners_r = cv2.findChessboardCorners(grey_r, (9,6), None)
-    if retval_r == 1:
-        objp_array_r.append(objpoints)
-        cornarray_r.append(corners_r)
+    # Storing image names in a list to call them later in the for loop:
+    images_l, images_r = [], []  # Lists for storing left and right images
+    for i in range(11):
+        images_l.append(f"left_{i}.png")
+        images_r.append(f"right_{i}.png")
 
-'''Calculating camera intrinsic parameters for right camera: '''
-ter_r, mtx_r, dist_r, rvecs_r, tvecs_r = cv2.calibrateCamera(objp_array_r, cornarray_r, (grey_r.shape[::-1]), None, None)
 
-r_cam_params = cv2.FileStorage(currwd.replace("code/task_1/project_2a_task1_ANP.py", "parameters/right_camera_intrinsics.xml"), cv2.FileStorage_WRITE)
-r_cam_params.write("Intrinsic_Matrix", mtx_r)
-r_cam_params.write("Distortion_Coefficients", dist_r)
+    dist_l = calibrate_camera("l", images_l, path)
+    dist_r = calibrate_camera("r", images_r, path)
 
-'''Checking calibration results for left camera: '''
-newcammat_r, roi_r = cv2.getOptimalNewCameraMatrix(mtx_r, dist_r, (w_r, h_r), 0, (w_r, h_r))
-mapx_r, mapy_r = cv2.initUndistortRectifyMap(mtx_r, dist_r, None, newcammat_r, (w_r, h_r), 5)
-rimg = cv2.imread(path+images_r[2])  # right_2.png
-dist_r = cv2.remap(rimg, mapx_r, mapy_r, cv2.INTER_LINEAR)
-x_r, y_r, w_r, h_r = roi_r
-dist_r = dist_r[y_r:y_r+h_r, x_r:x_r+w_r]
-
-chdir(currwd.replace("code/task_1/project_2a_task1_ANP.py", "output/task_1/"))
-cv2.imshow("calibration for left image 2", dist_l)
-cv2.imwrite("left_2_calibrated.png", dist_l)
-cv2.waitKey(0)
-cv2.imshow("calibration for right image 2", dist_r)
-cv2.imwrite("right_2_calibrated.png", dist_r)
-cv2.waitKey(0)
+    chdir(currwd.replace("code/task_1/project_2a_task1_ANP.py", "output/task_1/"))
+    cv2.imshow("calibration for left image 2", dist_l)
+    cv2.imwrite("left_2_calibrated.png", dist_l)
+    cv2.waitKey(0)
+    cv2.imshow("calibration for right image 2", dist_r)
+    cv2.imwrite("right_2_calibrated.png", dist_r)
+    cv2.waitKey(0)
